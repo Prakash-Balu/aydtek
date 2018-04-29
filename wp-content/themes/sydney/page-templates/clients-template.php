29b6b4ff2238a8e6bd05d28ca60ef01ff5fd6e37
unknown <prakash.engg89@gmail.com>
2018-04-29 12:50:10 +0530

    client changes

---
+++ wp-content/themes/sydney/page-templates/clients-template.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+
+Template Name: Clients Template
+
+*/
+	get_header();
+?>
+	<div class="col-sm-12 service_wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 service_container">
+					<h4 class="text-center text-uppercase">Our Clients</h4>
+					<div class="container service_content_container">
+						<div class="row">
+							<div class="col-sm-4">
+								<p class="text-center">ABC Company</p>
+								<img class="img-responsive" src="<?php echo get_template_directory_uri().'/images/services/web.jpg'?>">
+							</div>
+							<div class="col-sm-4">
+								<p class="text-center">ABC Company</p>
+								<img class="img-responsive" src="<?php echo get_template_directory_uri().'/images/services/web.jpg'?>">
+							</div>
+							<div class="col-sm-4">
+								<p class="text-center">ABC Company</p>
+								<img class="img-responsive" src="<?php echo get_template_directory_uri().'/images/services/web.jpg'?>">
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-4">
+								<p class="text-center">ABC Company</p>
+								<img class="img-responsive" src="<?php echo get_template_directory_uri().'/images/services/web.jpg'?>">
+							</div>
+							<div class="col-sm-4">
+								<p class="text-center">ABC Company</p>
+								<img class="img-responsive" src="<?php echo get_template_directory_uri().'/images/services/web.jpg'?>">
+							</div>
+							<div class="col-sm-4">
+								<p class="text-center">ABC Company</p>
+								<img class="img-responsive" src="<?php echo get_template_directory_uri().'/images/services/web.jpg'?>">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
+
